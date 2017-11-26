dbcc3ae9a8aaf58dbe056647d52ca71fb9b63549
LERUfic <aguelsw@gmail.com>
2017-11-26 22:44:30 +0700

    udah hampir jadi tapi masih nampilin yang hidden file

--- ls.c
+++ ls.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
-//#define _GNU_SOURCE
 #include <dirent.h>
 
-
-int main(){
-    char *path="/bin";
+void getdata(char path[]){
     DIR* dir;
     struct dirent* pdir;
     dir = opendir(path);
-    FILE *filenya=fopen("output.txt","w");
     while (pdir = readdir(dir)){
-        if ( !strcmp(pdir->d_name, ".") || !strcmp(pdir->d_name, "..") )
-        {
+        if ( !strcmp(pdir->d_name, ".") || !strcmp(pdir->d_name, "..") || pdir->d_name[0] == "."){
             // do nothing
-        } else{
-            fprintf(filenya,strcat(pdir->d_name,"\n"));
+        }
+        else{
+            printf("%s\n", pdir->d_name);
         }
     }
-   pid_t child_id;
-    child_id=fork();
-    if(child_id==0){
-        char *argv[5]= {"zip", "-rm", "output.zip","output.txt",NULL};
-        execv("/usr/bin/zip", argv);
+}
+
+
+int main(int argc, char *argv[]){
+    if(argc == 1){
+        getdata(".");
     }
+    else if(argc == 2){
+        getdata(argv[1]);
+    }
+    else{
+        int i;
+        for(i=1;i<argc;i++){
+            printf("%s\n",argv[i]);
+            getdata(argv[i]);
+        }
+    }
+
 }
