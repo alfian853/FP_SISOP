6ce62a4057b6e6b85e37ed46e1f89139e993d5fe
Unknown <alfian853@gmail.com>
2017-11-28 11:05:03 +0700

    FIX

--- factor.c
+++ factor.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-typedef long long LL;
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "fs.h"
 
-typedef struct{
-    LL f;//factor
+
+struct data{
+    int f;//factor
     int p;//power of factor
-}data;
+};
 /*
 --help
 --version
@@ -28,7 +30,7 @@ void check_option(char *arg){
     }
 
     if(valid){
-        printf("Usage: factor [NUMBER]...\n"
+        printf(1,"Usage: factor [NUMBER]...\n"
                "   or:  factor OPTION\n"
                "Print the prime factors of each specified integer NUMBER.  If none\n"
                "are specified on the command line, read them from standard input.\n"
@@ -37,7 +39,7 @@ void check_option(char *arg){
                "    --version  output version information and exit\n"
         );
 
-        exit(0);
+        exit();
     }
     else if( strlen(arg) < 10 ){
         valid = 1;
@@ -47,100 +49,89 @@ void check_option(char *arg){
     }
 
     if(valid){
-        printf("factor , Version : 1.0\n"
+        printf(1,"factor , Version : 1.0\n"
                "This is free software: you are free to change and redistribute it.\n"
                "\n"
                "Written by Operation System final project Team 3 (FPE 3)\n");
 
-        exit(0);
+        exit();
     }
 
-    printf("factor: unrecognized option '%s'\n",arg);
-    puts("Try 'factor --help' for more information.");
-    exit(0);
+    printf(1,"factor: unrecognized option '%s'\n",arg);
+    printf(1,"Try 'factor --help' for more information.");
+    exit();
 
 }
 
 int main(int argc,char *argv[]){
-    data fp[500];
-    LL nf;
-    LL k;
-    LL tf;
-    LL d;
-    LL n;
-    LL input[argc];
-    int num_count=0;
+    int k=0;
+    int tf=0;
+    int d=0;
+    int n=0;
+    int factor=-1;
+    int input[argc];
 
-//option checking
+    int num_count=0;
+////option checking
 for(int c=1;c<argc;++c){
 
     if(argv[c][0]=='-'){
         if(argv[c][1]=='-'){check_option(argv[c]);}
         else if( strlen(argv[c]) > 1 ){
-            printf("factor: invalid option -- '%c'\n",argv[c][1]);
-            puts("Try 'factor --help' for more information.");
+            printf(1,"factor: invalid option -- '%c'\n",argv[c][1]);
+            printf(1,"Try 'factor --help' for more information.\n");
         }
     }
 }
-//valid positive integer checking
+////valid positive integer checking
 for(int c=1;c<argc;++c){
-
-    n = atoll(argv[c]);
+    n = atoi(argv[c]);
     if(n==0 && strcmp(argv[c],"0") ){
-        printf("factor: \‘%s\’ is not a valid positive integer\n",argv[c]);
+        printf(1,"factor: ‘%s’ is not a valid positive integer\n",argv[c]);
     }
     else{
         input[num_count++] = n;
     }
-
 }
 for(int c=0;c<num_count;++c){
-
     n = input[c];
-    printf("%lld:",n);
-    nf = 0;
+    printf(1,"%d:",n);
     k = 0;
     tf = 2;
     d = 1;
-    fp[0].f=0;
-    fp[0].p=0;
 
     if(n > 1){
 
-    while(tf*tf <= n){                 // main loop
+    while(tf*tf <= n){
         if(n%tf == 0){
-
-            if(tf == fp[k].f){fp[k].p++;}
+            if(tf == factor){printf(1," %d",tf);}
             else{
                 k++;
-                fp[k].f = tf;
-                fp[k].p = 1;
+                printf(1," %d",tf);
+                factor=tf;
             }
-            n = n/tf;                        // compute new n
+            n = n/tf;
             continue;
         }
 
-        tf = tf + d;                       // increment trial factor
+        tf = tf + d;
         if(tf < 7)d = 2;
         else d = 6-d;
-    }                                    // end main loop
-
-        if( n == fp[k].f){
-            fp[k].p+=1;
-        }
-        else {
-            k++;
-            fp[k].f = n;
-            fp[k].p = 1;
-        }
-
-    for(int i=1; i<=k; i++){
-        while(fp[i].p--){
-            printf(" %lld",fp[i].f);
-        }
     }
-    printf("\n");
-    }//main else close
+
+    if( n == factor){
+        printf(1," %d",tf);
+    }
+    else {
+        k++;
+        factor=n;
+        printf(1," %d",n);
+    }
+    printf(1,"\n");
+    }//end main else
 }//argc loop
-    return 0;
+
+
+    exit();
 }
+
