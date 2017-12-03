83b083336006833e2927eb425944cbdaafb6b2b4
Unknown <fwildanf@gmail.com>
2017-12-03 21:00:02 +0700

    FIX

--- sleep.c
+++ sleep.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[]){
         printf(2, "Usage: sleep seconds\n");
         exit();
     }
+    else if(argc>2){
+        printf(2, "Invalid internal %s\n", argv[2]);
+        exit();
+    }
     int sleepsec=atoi(argv[1])*100;
     if(sleepsec>0) sleep(sleepsec);
     else printf(2, "Invalid internal %s\n", argv[1]);
